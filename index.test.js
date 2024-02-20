2a02c0fab040679b0accd5a607f058cafcfe0939
Evan Holloway <evan.holloway@alaskaair.com>
2024-02-20 09:50:52 -0800

    GETs added for spefic musicians

--- index.test.js
+++ index.test.js
@@ -13,10 +13,78 @@ const seedMusician = require("./seedData");
 describe('./musicians endpoint', () => {
     // Write your tests here
     
+    it("Testing musician endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
     
+    it("Testing musician response objects", async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0].id).toBe(1);
+        expect(responseData[1].id).toBe(2);
+        expect(responseData[2].id).toBe(3);
+
+        expect(responseData[0].name).toBe("Mick Jagger");
+        expect(responseData[1].name).toBe("Drake");
+        expect(responseData[2].name).toBe("Jimi Hendrix");
+
+        expect(responseData[0].instrument).toBe("Voice");
+        expect(responseData[1].instrument).toBe("Voice");
+        expect(responseData[2].instrument).toBe("Guitar");
+    })
+});
+
+describe("./musicians/x endpoint", () => {
+
+    it("Testing musician/1 endpoint", async () => {
+        const response = await request(app).get("/musicians/1");
+        expect(response.statusCode).toBe(200);
+    });
+
+    it("Testing musician/2 endpoint", async () => {
+        const response = await request(app).get("/musicians/2");
+        expect(response.statusCode).toBe(200);
+    });
+
+    it("Testing musician/3 endpoint", async () => {
+        const response = await request(app).get("/musicians/3");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("Testing ./musicians/x reponse objects", () => {
 
 
+    it("testing ./musician/1 reponse object", async () => {
 
+        const response = await request(app).get("/musicians/1");
+        const responseData = JSON.parse(response.text);
 
-    
-})
+        expect(responseData.id).toBe(1);
+        expect(responseData.name).toBe("Mick Jagger");
+        expect(responseData.instrument).toBe("Voice");
+    });
+
+    it("testing ./musician/2 reponse object", async () => {
+        
+        const response = await request(app).get("/musicians/2");
+        const responseData = JSON.parse(response.text);
+
+        expect(responseData.id).toBe(2);
+        expect(responseData.name).toBe("Drake");
+        expect(responseData.instrument).toBe("Voice");
+    });
+
+    it("testing ./musician/3 reponse object", async () => {
+        
+        const response = await request(app).get("/musicians/3");
+        const responseData = JSON.parse(response.text);
+
+        expect(responseData.id).toBe(3);
+        expect(responseData.name).toBe("Jimi Hendrix");
+        expect(responseData.instrument).toBe("Guitar");
+    });
+
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
 const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { db } = require("../db/connection");
+const { json } = require("sequelize");
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+})
 
+app.get("/musicians/1", async (req, res) => {
+    const musician = await Musician.findByPk(1);
+    res.json(musician);
+});
 
+app.get("/musicians/2", async (req, res) => {
+    const musician = await Musician.findByPk(2);
+    res.json(musician);
+});
 
-
-
-
+app.get("/musicians/3", async (req, res) => {
+    const musician = await Musician.findByPk(3);
+    res.json(musician);
+});
 
 module.exports = app;
